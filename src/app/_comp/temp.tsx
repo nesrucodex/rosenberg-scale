ffcec3eb3a150f7c06508618a2775ef71e5a61d0
Nesredin Getahun <nesrucodex01@gmail.com>
2024-05-24 16:21:58 +0300

    Rosenberg Scale Landing page

---
+++ src/app/_comp/temp.tsx
@@ -0,0 +1,14 @@
+
+import Hero from "./hero"
+import Feature from "./feature"
+import Footer from "./footer"
+
+export default function Component() {
+  return (
+    <>
+      <Hero />
+      <Feature />
+      <Footer />
+    </>
+  )
+}
